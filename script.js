022dc66310de6570a551e28f6aaec693f0675cb2
André-git <andre.potthast.ap@gmail.com>
2023-05-10 11:43:39 +0200

    first organisation code layout

---
+++ script.js
@@ -0,0 +1,34 @@
+// GameBoard object use module store as an array
+const gameBoard=(()=>{
+const board=["","","","","","","","",""]
+
+function renderTo(){
+    
+}
+return{
+    board
+}
+})();
+
+// Player objects factory
+
+
+// displayController 
+
+// gameFlow Object? use module
+
+
+// render game board function maybe as a method in gameboard?
+
+
+//
+
+
+const startBTN = document.querySelector("#start");
+startBTN.addEventListener("click",(e)=>{
+    console.log(e.target)
+})
+const restartBTN = document.querySelector("#restart");
+restartBTN.addEventListener("click",(e)=>{
+    console.log(e.target)
+})
