9c7c7edf450b0de23d26c3463c3ceaad519e320d
André-git <andre.potthast.ap@gmail.com>
2023-06-08 20:06:46 +0200

    svg marks working, but need change

--- script.js
+++ script.js
@@ -1,21 +1,59 @@
+import { Omark, Xmark } from "./svg.js";
+// console.log(Xmark[0])
+// console.log(Omark[0])
 // GameBoard object  module store
 const GameBoard = (() => {
-  let board = ["", "", "", "", "", "", "", "", ""];
+  const board = ["", "", "", "", "", "", "", "", ""];
 
   const htmlBoard = document.querySelector("#gameboard");
 
   function resetBoard() {
-    for (let index = 0; index < board.length; index++) {
+    for (let index = 0; index < board.length; index += 1) {
       board[index] = "";
     }
   }
   // click handler
   // ? move to  game flow logic
   function clickHandler(event) {
-    event.target.innerText = Game.activeMark(); //
+    // event.target.innerText = Game.activeMark(); //
+    event.target.appendChild(createMark());
+
     board[event.target.dataset.index] = Game.activeMark();
     Game.nextPlayer();
     checkForWin(board);
+  }
+  function createMark() {
+    const markArr=(Game.activeMark()==="X")?Xmark:Omark;
+    const randomNum=Math.floor(Math.random() * markArr.length)
+    return createSvg(markArr[randomNum]);
+  }
+  function createSvg(arr) {
+    const iconSvg = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "svg"
+    );
+
+    iconSvg.setAttribute("width", "180");
+    iconSvg.setAttribute("height", "180");
+
+    // x mark needs more then one path
+
+    arr.forEach((obj) => {
+      const iconPath = document.createElementNS(
+        "http://www.w3.org/2000/svg",
+        "path"
+      );
+      iconPath.setAttribute("stroke-linecap", "round");
+      iconPath.setAttribute("stroke-linejoin", "round");
+      iconPath.setAttribute("fill", "none");
+      // only these attribute will be different
+      iconPath.setAttribute("d", `${obj.d}`);
+      iconPath.setAttribute("stroke-width", `${obj.strokeWidth}`);
+      iconPath.setAttribute("stroke", `${obj.stroke}`);
+
+      iconSvg.appendChild(iconPath);
+    });
+    return iconSvg;
   }
 
   // display board
@@ -71,13 +109,17 @@ const Game = (() => {
   };
 })();
 
+//
+
 function checkForWin(board) {
   function threeOfKind(first, second, third) {
     if (first === "") {
       return;
     }
     if (first === second && first === third) {
-      document.querySelector(".gameMessage").textContent = `${Game.getPlayer()} won the Game`;
+      document.querySelector(
+        ".gameMessage"
+      ).textContent = `${Game.getPlayer()} won the Game`;
     }
   }
 
